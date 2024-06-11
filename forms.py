3a8551099585551d6fc2014c9be350d16daedfb5
Mateusz Wojciechowski <120648222+Mateusz-Wojciechowski@users.noreply.github.com>
2024-06-11 16:57:22 +0200

    działa wyszukiwanie po adresach

--- forms.py
+++ forms.py
@@ -8,10 +8,3 @@ class DirectionsForm(FlaskForm):
     end_point = StringField('End Point', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-
-class CoordinatesForm(FlaskForm):
-    start_lat = DecimalField('Start Latitude', validators=[DataRequired()])
-    start_lon = DecimalField('Start Longitude', validators=[DataRequired()])
-    end_lat = DecimalField('End Latitude', validators=[DataRequired()])
-    end_lon = DecimalField('End Longitude', validators=[DataRequired()])
-    submit = SubmitField('Generate Route')
